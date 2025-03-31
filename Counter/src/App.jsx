57379bd5e7a450dd2fb39c94b64e53346af87f53
pinkec-pvt457 <pankajchoudhari457@gmail.com>
2025-03-31 10:08:22 +0530

    reverted due to not working properly

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -5,24 +5,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
 function App() {
-
-  useEffect(() => {
-
-    const metaThemeColor = document.querySelector("meta[name=theme-color]");
-    if (metaThemeColor) {
-      metaThemeColor.setAttribute("content", "#00000000");
-    }
-
-    const hideNavBar = () => {
-      window.scrollTo(0, 1);
-    };
-
-    hideNavBar();
-    window.addEventListener("resize", hideNavBar);
-
-    return () => window.removeEventListener("resize", hideNavBar);
-  }, []);
-
+  
   return (
     <div className='h-screen min-h-screen w-screen flex flex-col justify-between bg-indigo-400/50 dark:bg-gray-950'>
       <Navbar />
