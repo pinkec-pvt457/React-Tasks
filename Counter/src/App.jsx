895bcde42aca1945cd197881c5a61b7ec1a85306
pinkec-pvt457 <pankajchoudhari457@gmail.com>
2025-03-31 17:35:22 +0530

    Final Commit

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import React, { useEffect } from 'react'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
@@ -7,7 +6,7 @@ import Footer from './components/Footer'
 function App() {
   
   return (
-    <div className='h-screen min-h-screen w-screen flex flex-col justify-between bg-indigo-400/50 dark:bg-gray-950'>
+    <div className='h-screen min-h-screen w-screen overflow-x-hidden overflow-y-auto flex flex-col justify-between bg-indigo-400/50 dark:bg-gray-950'>
       <Navbar />
       <Home />
       <Footer />
